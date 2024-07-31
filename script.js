e9a6f4f8dcf92b87368683104834d0b6ecb02eb3
Yan Naing Aung <yannaingaung2209@gmail.com>
2024-07-31 19:02:31 +0630

    chore :added student data

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("there was html big bone.");
+
+const student = {
+  name: "Mg Mg",
+  age: 20,
+  ownedVehicle: "bicycle",
+};
+
+console.log(`${student.name} have a ${student.ownedVehicle}`);
